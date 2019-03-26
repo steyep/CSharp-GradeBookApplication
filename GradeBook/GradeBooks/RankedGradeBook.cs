559bf631d025b54b38092918aca76d36f28a20a8
Stephen Pennell <stepenn7@gmail.com>
2019-03-26 16:55:08 -0400

    Completes "Override Statistics"

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,13 +17,30 @@ namespace GradeBook.GradeBooks
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
 
             char[] grades = new char[] { 'A', 'B', 'C', 'D', 'F' };
-            var index = Students.OrderByDescending(e => e.AverageGrade)
-                // .Select(e => e.AverageGrade)
-                .ToList()
-                .FindIndex(e => e.AverageGrade == averageGrade);
-
+            var index = Students.OrderByDescending(e => e.AverageGrade).ToList().FindIndex(e => e.AverageGrade == averageGrade);
             var threshold = Math.Ceiling(Students.Count * 0.2);
             return grades[(int) Math.Floor(index / threshold)];
         }
+
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+        }
+
     }
 }
